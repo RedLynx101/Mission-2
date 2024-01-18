9d42d9a41f27bf4f0cf443c4b1820d36c34cadde
RedLynx101 <NoahHicks101@gmail.com>
2024-01-17 23:59:37 -0700

    Updaye

--- Project2/Program.cs
+++ Project2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Noah Hicks
+// IS 413 1/17/24
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.Write("How many dice rolls would you like to simulate? ");
+        int numberOfRolls = int.Parse(Console.ReadLine());
+
+        DiceRoller roller = new DiceRoller();
+        int[] outcomes = roller.RollDice(numberOfRolls);
+
+        HistogramGenerator generator = new HistogramGenerator();
+        generator.GenerateHistogram(outcomes, numberOfRolls);
+    }
+}
